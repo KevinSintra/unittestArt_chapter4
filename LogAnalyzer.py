2dfbf464859c819f9d6f43e9296af6bab0717c3b
KevinPro <kevin79713@gmail.com>
2021-10-14 17:05:28 +0800

    commit 2: 程式功能新增, 也需增加測試. 且保留原始測試(虛設常式與模擬物件同時使用).

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -1,7 +1,12 @@
 import abc
 
 """
-情境: 紀錄 LOG 時若失敗, 需通知 web 服務.
+情境1: 紀錄 LOG 時若失敗, 需通知 web 服務. (commit 1)
+
+情境2: 功能新增 => 將 web service 互動使用 tyr catch 包住, 若互動失敗需要發信. (commit 2)
+    # 如何替換掉 web 服務?
+    # 如何模擬來自 web 服務引發的例外?
+    # 如何驗證發信過程是否正確?
 """
 
 
@@ -11,13 +16,24 @@ class IWebService(metaclass=abc.ABCMeta):
         pass
 
 
-class LogAnalyzer:
-    __webService__: IWebService
+class IEmailService(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def sendEmail(self, to: str, subject: str, body: str) -> None:
+        pass
 
-    def __init__(self, webService: IWebService) -> None:
+
+class LogAnalyzer:
+
+    def __init__(self, webService: IWebService, mailService: IEmailService = None) -> None:
         self.__webService__ = webService
+        self.__emailService__ = mailService
         pass
 
     def analyze(self, fileName: str) -> None:
-        if(len(fileName) < 8):
-            self.__webService__.logError("FileName too short: {name}".format(name=fileName))
+        try:
+            if(len(fileName) < 8):
+                self.__webService__.logError(
+                    "FileName too short: {name}".format(name=fileName))
+        except Exception as e:
+            self.__emailService__.sendEmail(
+                "someone@somewhere.com", "cant't log", e)
--- test_LogAnalyzer.py
+++ test_LogAnalyzer.py
@@ -1,5 +1,13 @@
 import pytest
-from LogAnalyzer import IWebService, LogAnalyzer
+from LogAnalyzer import IEmailService, IWebService, LogAnalyzer
+
+
+class FakeWebService(IWebService):
+    lastError: str
+
+    def logError(self, message: str) -> None:
+        self.lastError = message
+
 
 def test_Analyze_TooShortFileName_CallWebService():
     fakeService = FakeWebService()
@@ -9,11 +17,39 @@ def test_Analyze_TooShortFileName_CallWebService():
     exceptStr = "FileName too short: {name}".format(name=shortFileName)
     assert exceptStr == fakeService.lastError
 
-class FakeWebService(IWebService):
-    lastError: str
+
+class FakeWebService2(IWebService):
+    toThrow: Exception = None
 
     def logError(self, message: str) -> None:
-        self.lastError = message
+        if(self.toThrow != None):
+            raise self.toThrow
+        else:
+            pass
 
 
+class FakeEmailService(IEmailService):
+    def __init__(self) -> None:
+        self.to = ""
+        self.subject = ""
+        self.body = ""
+        pass
 
+    def sendEmail(self, to: str, subject: str, body: str) -> None:
+        self.to = to
+        self.subject = subject
+        self.body = body
+        pass
+
+
+def test_Analyzer_WebServiceThrows_SendEmail():
+    webService = FakeWebService2()  # 因不驗證, 所以是 虛設常式
+    e = Exception("fake exception")
+    webService.toThrow = e
+    mailService = FakeEmailService()  # 因需驗證, 所以是 模擬物件
+    log = LogAnalyzer(webService, mailService)
+    fileName = "abc.txt"
+    log.analyze(fileName)
+    assert "someone@somewhere.com" == mailService.to
+    assert "cant't log" == mailService.subject
+    assert e == mailService.body
